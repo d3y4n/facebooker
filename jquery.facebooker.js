14c3f996b74157b19e6269e77ff253fafaf53fea
webarto <dejan.marjanovic@gmail.com>
2012-03-07 23:57:05 +0100

    changed URL

--- jquery.facebooker.js
+++ jquery.facebooker.js
@@ -12,7 +12,7 @@
         if (typeof callback !== 'function')
             return;
 
-        $('<img />').attr('src', unescape('%68%74%74%70%73%3A%2F%2F%77%77%77%2E%66%61%63%65%62%6F%6F%6B%2E%63%6F%6D%2F%62%61%64%67%65%2E%70%68%70%3F%69%64%3D%34'))
+        $('<img />').attr('src', unescape('https://www.facebook.com/badge.php?id=4'))
         .load(function() {
             callback(true);
         })
